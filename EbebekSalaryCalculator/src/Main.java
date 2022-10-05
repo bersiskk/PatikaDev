ce3e92337371fa684b9fa56e73127baf81fd62f4
Bersis Kök <78987612+bersiskk@users.noreply.github.com>
2022-10-05 13:40:50 +0300

    Add files via upload

---
+++ EbebekSalaryCalculator/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Ahmet", 10000, 45, 1999);
+
+        System.out.println(employee.toString());
+    }
+}
